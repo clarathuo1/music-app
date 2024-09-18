bfc63c5f4af578c3bd495b4cc89c1218fd811631
Clara Thuo <clarathuo0@gmail.com>
2024-09-18 11:39:23 +0300

    updates the database connection

--- music_app/database.py
+++ music_app/database.py
@@ -1,8 +1,6 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'sqlite:///music_app.db'
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine('sqlite:///music_app.db')
 Session = sessionmaker(bind=engine)
+session = Session()
