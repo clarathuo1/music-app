1a6e0a141036141f1bca3cb05e1b7ab2079732c4
Clara Thuo <clarathuo0@gmail.com>
2024-09-19 12:41:15 +0300

    ensures functionality

--- music_app/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('sqlite:///music_app.db')
-Base = declarative_base()
-
-Session = sessionmaker(bind=engine)
-session = Session()
